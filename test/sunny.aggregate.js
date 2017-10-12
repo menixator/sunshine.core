a861ecfc1fbd2a6b272b676724dec06df5e4e688
menixator <ahmed.miljau@gmail.com>
2017-10-12 16:12:49 +0500

    Add new aggregation method and accomodate new plants inthe responses

---
+++ test/sunny.aggregate.js
@@ -0,0 +1,22 @@
+const Sunshine = require("../lib/sunshine");
+const config = require('../lib/config').fromEnv();
+
+let bot = new Sunshine({
+  username: config.get('username'),
+  password: config.get('password')
+});
+
+bot
+  .login()
+  .then(async () => {
+    let powerReading = await bot.aggregate();
+    console.log(powerReading)
+    return bot.logout();
+  })
+  .catch(err => {
+    console.error(err);
+    return bot.logout();
+  })
+  .catch(err => {
+    console.error(err);
+  });
